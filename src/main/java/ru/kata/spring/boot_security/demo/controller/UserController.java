9e98df90d5a3f4136f11d1fc4f0753c8c98b8b20
AnimalO <o.loginoff@gmail.com>
2023-05-01 20:29:27 +0300

    Добавил адекватное окно вывода информации пользователя

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -1,13 +1,8 @@
 package ru.kata.spring.boot_security.demo.controller;
 
-//import jakarta.validation.Valid;
-import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
@@ -17,15 +12,18 @@ import java.security.Principal;
 
 @Controller
 public class UserController {
-    @GetMapping(value = "/user")
-//    public String userPage(ModelMap model) {
-//        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        model.addAttribute("user", user);
-//        return "user";
-//    }
 
-    public String x3 (Principal principal) {
-        System.out.println(principal.getName());
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "/user")
+    public String getUser(Model model, Principal principal) {
+        User user = userService.findByName(principal.getName());
+        model.addAttribute("user", user);
         return "user";
     }
 }
