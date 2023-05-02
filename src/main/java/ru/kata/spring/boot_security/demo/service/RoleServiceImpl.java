01bf2fc2edc71d0ed93623ccd952157be51c877b
AnimalO <o.loginoff@gmail.com>
2023-05-02 23:46:43 +0300

    Новые классы/интерфейсы

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
@@ -0,0 +1,34 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public RoleServiceImpl(RoleRepository rolesRepository) {
+        this.roleRepository = rolesRepository;
+    }
+
+    public Role findOne(int id) {
+        Optional<Role> role = roleRepository.findById((long) id);
+        return role.orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
+    }
+
+}
