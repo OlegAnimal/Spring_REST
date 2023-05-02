f1ccf20948d2de3cd05449e90453f3edbddbbf6e
AnimalO <o.loginoff@gmail.com>
2023-05-02 23:45:56 +0300

    Код стайл

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -6,15 +6,14 @@ import javax.persistence.*;
 import java.util.Set;
 
 @Entity
-@Table (name = "securiry_roles")
-public class Role implements GrantedAuthority{
+@Table(name = "security_roles")
+public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "name")
     private String name;
-
-//    @ManyToMany(mappedBy = "roles")
-//    private Set<User> users;
 
     public Role() {
     }
@@ -43,14 +42,6 @@ public class Role implements GrantedAuthority{
     public void setName(String name) {
         this.name = name;
     }
-
-//    public Set<User> getUsers() {
-//        return users;
-//    }
-//
-//    public void setUsers(Set<User> users) {
-//        this.users = users;
-//    }
 
     public String getAuthority() {
         return name;
