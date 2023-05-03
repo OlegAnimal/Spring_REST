7d545a10356395f96474bab9e1565a60b81a82df
AnimalO <o.loginoff@gmail.com>
2023-05-03 18:05:56 +0300

    loadUserByUsername возвращает просто user. Удалил теперь не нужный метод mapRolesToAuthorities. Поправил аннотации @Transactional и @Override. Добавил имплементацию UserDetailsService. В методе save убрал явное присвоение роли User пользователям.

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -12,37 +13,35 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.repositories.UserRepository;
-
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-    private final RoleService roleService;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder, RoleService roleService) {
+    public UserServiceImpl(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
-        this.roleService = roleService;
     }
 
+    @Override
     public List<User> findAll() {
         return userRepository.findAll();
     }
 
     @Transactional
+    @Override
     public void save(User user) {
         if (userRepository.findByName(user.getName()) != null) return;
-        user.setRoles(Collections.singleton(roleService.findOne(1)));
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 
     @Transactional
+    @Override
     public void update(Long id, User updatedUser) {
         updatedUser.setId(id);
         updatedUser.setPassword(passwordEncoder.encode(updatedUser.getPassword()));
@@ -50,31 +49,27 @@ public class UserServiceImpl implements UserService {
     }
 
     @Transactional
+    @Override
     public void delete(Long id) {
         userRepository.deleteById(id);
     }
 
+    @Override
     public User findOne(Long id) {
         Optional<User> foundUser = userRepository.findById(id);
         return foundUser.orElse(null);
     }
 
     @Override
-    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByName(username);
-
         if (user == null) {
             throw new UsernameNotFoundException("User not found");
         }
-
-        return new org.springframework.security.core.userdetails.User(user.getName(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+        return user;
     }
 
-    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
-        return roles.stream().map(r -> new SimpleGrantedAuthority(r.getName())).collect(Collectors.toList());
-    }
-
+    @Override
     public User findByName(String username) {
         return userRepository.findByName(username);
     }
